458a696d2f86edc34f944587d302a5f1a567796b
lle1ya <AlexTV2@yandex.ru>
2024-10-31 10:44:57 +0500

    Initial commit

---
+++ src/main/java/ru/shvyrkov/MyUiRestDbService/dao/StudentRepository.java
@@ -0,0 +1,9 @@
+package ru.shvyrkov.MyUiRestDbService.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.shvyrkov.MyUiRestDbService.entity.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
